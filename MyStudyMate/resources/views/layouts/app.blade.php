db4b54ed056a755f1ef49d4aec5efe2d9dbf1713
akram-anissi <148265887+akram-anissi@users.noreply.github.com>
2023-12-29 03:17:01 +0100

    header

--- MyStudyMate/resources/views/layouts/app.blade.php
+++ MyStudyMate/resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -76,5 +76,106 @@
             @yield('content')
         </main>
     </div>
+</body>
+</html> --}}
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="resources/css/header.css">
+    <link rel="stylesheet" href="resources/css/Normalize.css">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/Normalize.css','resources/css/header.css'])
+</head>
+<body>
+    <header class="header-container">
+        <!-- logo -->
+        <h1 class="logo"><a class="navbar-brand" href="{{ url('/') }}">MyStudyMate!!</a></h1>
+        <!-- nav bar -->
+        <div class="nav-bar">
+            <ul class="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Emplois du temps</a></li>
+                <li><a href="#">Our Service</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+            <!-- sing in and sing up -->
+            <ul class="sign">
+                @guest
+                            @if (Route::has('login'))
+                            <li><a href="{{ route('login') }}">Sign in</a></li>
+                            @endif
+
+                            @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}">Sign up</a></li>
+                            @endif
+
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+            </ul>
+        </div>
+        <!-- menu burger for small screen -->
+        <div class="menuConatiner">
+
+            <div class="menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </header>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+        // Select elements
+        const menuIcon = document.querySelector(".menuConatiner");
+        const navBar = document.querySelector(".nav-bar");
+        const span1 = document.querySelector(".menu span:first-child");
+        const span2 = document.querySelector(".menu span:nth-child(2)");
+        const span3 = document.querySelector(".menu span:last-child");
+
+        // Add click event listener to the menu icon
+        menuIcon.addEventListener("click", (event)=> {
+            // Toggle the visibility of the navigation bar
+                navBar.classList.toggle('active');
+                span1.classList.toggle('firstmenu');
+                span3.classList.toggle('lastmenu');
+                span2.classList.toggle('span2');
+        });
+
+
+        });
+    </script>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
 </body>
 </html>
