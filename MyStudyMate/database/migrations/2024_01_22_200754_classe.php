0384d17c9bcec7c26bbb451facfe4fcd0e4a6c4c
hibakenafi <kenafi.hiba@etu.uae.ac.ma>
2024-01-22 22:27:21 +0100

    Update 2024_01_22_200754_classe.php

--- MyStudyMate/database/migrations/2024_01_22_200754_classe.php
+++ MyStudyMate/database/migrations/2024_01_22_200754_classe.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('modules', function (Blueprint $table) {
+        Schema::create('Classe', function (Blueprint $table) {
             $table->id('id_classe');
             $table->integer('nbrEtudiants');
             $table->integer('id_Module');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('Classe');
     }
 };
