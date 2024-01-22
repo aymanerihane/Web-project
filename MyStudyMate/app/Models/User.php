79161c3e87e91990e952c0009fcb6accffa161b1
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2024-01-22 21:27:29 +0100

    Revert "immigrations"
    This reverts commit 78122d6400b2feb1ca6f2a9dd2f8589fea5a25ec, reversing
    changes made to b2e720b0d009627e89d307170718d01b415f6e7e.

--- MyStudyMate/app/Models/User.php
+++ MyStudyMate/app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'is_role'
+        'is_role',
+        'is_delegue',
+        'is_chef'
     ];
 
     /**
