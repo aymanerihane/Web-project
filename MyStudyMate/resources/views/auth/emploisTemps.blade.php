736883fbfa88b44f1dd791d01ade3281bafe59f8
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2024-01-24 22:06:17 +0100

    update

--- MyStudyMate/resources/views/auth/emploisTemps.blade.php
+++ MyStudyMate/resources/views/auth/emploisTemps.blade.php
@@ -1,4 +1,39 @@
 
+<div class="signbox" style="position: relative;margin-bottom:76px">
+    <form style="width:100%;" class="formSign" method="POST" action="">
+            <div style="display: flex;flex-direction:column;justify-content: space-between;align-items: center;">
+
+                <div class="email">
+                    <input id="titleEmplois" type="text" name="titleEmplois" required >
+                    <label class="labelf">
+                        <span style="transition-delay:0ms">T</span><span style="transition-delay:50ms">I</span><span style="transition-delay:100ms">T</span><span style="transition-delay:150ms">R</span><span style="transition-delay:200ms">E</span>
+
+                </div>
+                    <Label>Jour:</Label><br>
+                    <span class="custom-dropdown small" >
+                        <Select class="select" name="groupTP" style="margin-bottom: 15px;">
+                            <option class="option" value="" disabled selected>Jour</option>
+                            <option class="option" value=""></option>
+                        </Select>
+                    </span>
+                    <Label>Creneau :</Label><br>
+                    <span class="custom-dropdown small" >
+                        <Select class="select" name="groupTP" style="margin-bottom: 15px;">
+                            <option class="option" value="" disabled selected>XX:XX-XX:XX</option>
+                            <option class="option" value="" ></option>
+                        </Select>
+                    </span>
+
+                <div class="sb">
+                    <button type="submit">
+                        <div style="margin-bottom: 0" class="btnLogin"><span>ADD</span></div>
+                        </button>
+
+                </div>
+        </div>
+    </form>
+</div>
+<h1 class="h1">Emplois du temps</h1>
 <table>
     <?php
         $jour = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
