5d10790a02f35a7f005f31825c2fa2f72aa88dbf
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2024-01-31 01:02:14 +0100

    update

---
+++ MyStudyMate/resources/views/auth/addModule.blade.php
@@ -0,0 +1,63 @@
+
+<div class="manup">
+    <form id="afctform">
+        @csrf
+    <div class="gestion">
+        <div class="salle">
+                <Label>Nom Filiere :</Label><br>
+                <input type="text" name="nomFiliere" required>
+
+        </div>
+        <div class="salle">
+            <Label>Contenu Filiere :</Label><br>
+            <input type="text" name="Contenu" required>
+
+    </div>
+
+
+</div>
+<div style="width: 100%;display:flex;justify-content: center;">
+
+    <div class="departement">
+        <Label>Departements</Label><br>
+        <span class="custom-dropdown small">
+            <select name="departement" required>
+                @php
+                $departements = app('App\Http\Controllers\Departements')->showDepartements();
+                @endphp
+                @foreach ($departements as $departement)
+                <option class="option" value="{{ $departement->id_departement }}">{{ $departement->nom }}</option>
+                @endforeach
+            </select>
+        </span>
+    </div>
+</div>
+{{-- <div style="width: 100%;display:flex;justify-content: center;">
+
+    <div class="departement">
+        <Label>filiere</Label><br>
+        <span class="custom-dropdown small">
+            <select name="respFiliere" required>
+                @php
+                $departements = app('App\Http\Controllers\proffesseur')->showFilieres();
+                @endphp
+                @foreach ($departements as $departement)
+                <option class="option" value="{{ $departement->id_departement }}">{{ $departement->nom }}</option>
+                @endforeach
+            </select>
+        </span>
+    </div>
+</div> --}}
+    <button class="asso" type="button" id="affectclasse" style="width: 100%;display:flex;justify-content: center;">
+        <div class="btnLogin"><span>ADD</span></div>
+    </button>
+</form>
+
+</div><br>
+<h1 class="h1">Liste des Filiere</h1>
+
+
+
+<div id="tabclasse">
+
+</div>
--- MyStudyMate/resources/views/prof/home.blade.php
+++ MyStudyMate/resources/views/prof/home.blade.php
@@ -16,37 +16,37 @@
                     <div class="nav-button btn1 active-side1 active-side"><span>Gérer Annonces Modules</span></div>
                     <div class="nav-button btn2"><span>Répondre Demandes Étudiants</span></div>
 
-                    <div id="footer-nav">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fefefe" d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>
-                        <ul class="sign foo">
-                            <p class="name"> {{ Auth::user()->name }} </p>
-                            <li class="btnlog">
-
-                                <div class="lougout">
-                                    <a class="loga" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-
-                        </ul>
-                    </div>
 
 
-                <div id="nav-content-highlight"></div>
-                <div id="nav-content-highlight2"></div>
-                @if(Auth::user()->is_chef == 1)
+                    <div id="nav-content-highlight"></div>
+                    <div id="nav-content-highlight2"></div>
+                    @if(Auth::user()->is_chef == 1)
                     <div class="nav-footer" style="z-index: 1999">
                         <a href="{{url('chefDep/choixMode')}}">Change Mode</a>
                     </div>
-                @endif
-            </div>
+                    @endif
+                </div>
+                <div id="footer-nav">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fefefe" d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>
+                    <ul class="sign foo">
+                        <p class="name"> {{ Auth::user()->name }} </p>
+                        <li class="btnlog">
+
+                            <div class="lougout">
+                                <a class="loga" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
             <input id="nav-footer-toggle" type="checkbox"/>
         </div>
 
